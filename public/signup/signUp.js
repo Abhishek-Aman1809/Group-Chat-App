b517a18bce0f9c3ec7a25898063e8850f7d7c340
Abhishek Aman <70830645+Abhishek-Aman1809@users.noreply.github.com>
2023-05-03 18:19:12 +0530

    Add files via upload

---
+++ public/signup/signUp.js
@@ -0,0 +1,31 @@
+async function signup(e) {
+  e.preventDefault();
+  const nameForm = document.getElementById("name").value;
+  const emailForm = document.getElementById("email").value;
+  const phoneForm = document.getElementById("phoneNo").value;
+  const passwordForm = document.getElementById("password").value;
+
+  await axios
+    .post(`http://localhost:4001/user/signup`, {
+      name: nameForm,
+      email: emailForm,
+      phone: phoneForm,
+      password: passwordForm,
+    })
+    .then((result) => {
+      if (result.status == 201) {
+        alert("Successfully signed up ! ");
+        window.location.href = "../login/login.html";
+      }
+    })
+    .catch((err) => {
+      console.log(err.response.status);
+      if (err.response.status == 401) {
+        var pop = document.getElementById("alreadyExist");
+        let p = document.createElement("p");
+        p.className = "message";
+        p.textContent = "* User already exists";
+        pop.appendChild(p);
+      }
+    });
+}
